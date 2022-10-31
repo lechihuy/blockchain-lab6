aca2c649d4d077a0de5014deb5b6fbb16a0a5a42
Lê Chí Huy <lechihuy1062001@gmail.com>
2022-10-31 17:18:51 +0700

    bai2

--- contracts/bai2.sol
+++ contracts/bai2.sol
@@ -1,24 +1,8 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
 contract Bai2 {
-    function execute(int a, int b, int c) public pure returns (int[3] memory) {
-        int[3] memory data;
-        
-        if (a <= b && b <= c) {
-            data = [a, b, c];
-        } else if (a <= c && c <= b) {
-            data = [a, c, b];
-        } else if (b <= a && a <= c) {
-            data = [b, a, c];
-        } else if (b <= c && c <= a) {
-            data = [b, c, a];
-        } else if (c <= b && b <= a) {
-            data = [c, b, a];
-        } else if (c <= a && a <= b) {
-            data = [c, a, b];
-        }
-
-        return data;
+    function execute(int n1, int n2, int n3, int n4, int n5) public pure returns (bool) {
+        return n1 == n5 && n2 == n4;
     }
 }
