94afd384a8cae81c89d8ce9b636db3bc9089af8b
Lê Chí Huy <lechihuy1062001@gmail.com>
2022-10-31 14:22:10 +0700

    bai1

--- contracts/bai1.sol
+++ contracts/bai1.sol
@@ -1,11 +1,17 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
 contract Bai1 {
-    function execute(int x, int y) public pure returns (int sum, int subtract, int mod, int integer) {
-        sum = x + y;
-        subtract = x - y;
-        mod = x % y;
-        integer = x - mod;
-    }
+    function execute(bytes1 b1, bytes1 b2, bytes1 b3, bytes1 b4, bytes1 b5) public pure returns (bytes1) {
+        bytes1[5] memory data = [b1, b2, b3, b4, b5];
+        bytes1 max = b1;
+
+        for (uint i = 0; i < data.length; i++) {
+            if (data[i] > max) {
+                max = data[i];
+            }
+        }
+
+        return max;
+    } 
 }
